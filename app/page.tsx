4fffde223c707dc3d18ca5dce1424e519fecd5f6
ajithpg <ajithpg008@gmail.com>
2025-08-08 22:02:46 +0530

    feat: add Hero and FeaturedResources components to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import { Button } from "@/components/ui/button";
+import FeaturedResources from "@/components/home/FeaturedResources";
+import Hero from "@/components/home/Hero";
+
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <Button variant="outline" size="lg" className="capitalize m-8">
-        Click Me
-      </Button>
-    </div>
+    <>
+       <Hero/>
+       <FeaturedResources/>
+    </>
   );
 }
