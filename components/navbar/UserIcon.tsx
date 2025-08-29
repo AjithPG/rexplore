24da49922b34d5046b92401029ab66d0f2cdd2c2
ajithpg <ajithpg008@gmail.com>
2025-08-29 13:44:21 +0530

    feat : add signin and signup feature in navbar

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,9 +1,16 @@
+import { auth, currentUser } from "@clerk/nextjs/server"
+import { RiUserLine } from "@remixicon/react";
 
-
-const UserIcon = () => {
-  return (
-    <div>UserIcon</div>
-  )
+const UserIcon = async() => {
+  //const {userID} = auth();
+  const user = await currentUser();
+  const profileImg = user?.imageUrl;
+  if(profileImg){
+    return (
+      <img src={profileImg} alt="user" className="w-6 h-6 rounded-full object-cover"/>
+    )
+  }
+  return <RiUserLine className="w-6 h-6 rounded-full bg-primary text-white"/>
 }
 
 export default UserIcon
