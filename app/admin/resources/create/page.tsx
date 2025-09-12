318065677dbf9db4c66f1f3923c30614f9b3da8c
ajithpg <ajithpg008@gmail.com>
2025-09-12 22:02:26 +0530

    feat : admin pages

---
+++ app/admin/resources/create/page.tsx
@@ -0,0 +1,9 @@
+
+
+const CreateResourcePage = () => {
+  return (
+    <div> Create Resource</div>
+  )
+}
+
+export default CreateResourcePage;
---
+++ app/admin/resources/page.tsx
@@ -0,0 +1,5 @@
+const ResourcePage = () => {
+  return <div>Admin Resource Page</div>;
+};
+
+export default ResourcePage;
